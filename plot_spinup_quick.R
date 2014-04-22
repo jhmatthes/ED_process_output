9265615468eaab9b624699845d6b2fb12f0b5990
jhmatthes <jaclyn.hatala.matthes@gmail.com>
2014-04-22 16:17:39 -0400

    quick ED2 plotting

---
+++ plot_spinup_quick.R
@@ -0,0 +1,126 @@
+#This code processes and plots annual output from the ED2 model spinup period
+#Jaclyn Hatala Matthes, 1/30/14
+#jaclyn.hatala.matthes@gmail.com
+
+#Load libraries
+ok = require(chron,lib.loc="/usr4/spclpgm/jmatthes/"); if (! ok) stop("Package chron is not available...")
+ok = require(ncdf,lib.loc="/usr4/spclpgm/jmatthes/") ; if (! ok) stop("Package ncdf is not available...")
+ok = require(maps,lib.loc="/usr4/spclpgm/jmatthes/") ; if (! ok) stop("Package maps is not available...")
+ok = require(sp,lib.loc="/usr4/spclpgm/jmatthes/") ; if (! ok) stop("Package raster is not available...")
+ok = require(raster,lib.loc="/usr4/spclpgm/jmatthes/") ; if (! ok) stop("Package raster is not available...")
+ok = require(colorspace,lib.loc="/usr4/spclpgm/jmatthes/") ; if (! ok) stop("Package raster is not available...")
+
+#Set sites
+sites <- c("PBL","PHA","PMB","PDL","PHO","PUN")
+site.names <- c("Billy's Lake","Harvard Forest","Minden Bog","Demming Lake","Howland Forest","UNDERC")
+
+#Names and colors for all PFTs, so it works for tropical and temperate.
+pft.names = c("C4 grass"          ,"Early tropical"    ,"Mid tropical"      
+              ,"Late tropical"     ,"Temperate C3 Grass","North Pine"        
+              ,"South Pine"        ,"Late conifer"      ,"Early hardwood"    
+              ,"Mid hardwood"      ,"Late hardwood"     ,"C3 crop"           
+              ,"C3 pasture"        ,"C4 crop"           ,"C4 pasture"        
+              ,"C3 grass"          ,"Araucaria"         ,"Total"             )
+pft.cols  = c("gold"              ,"chartreuse"        ,"chartreuse4"       
+              ,"#004E00"           ,"mediumpurple1"     ,"deepskyblue"       
+              ,"mediumturquoise"   ,"royalblue4"        , "darkorange"       
+              ,"orangered"         ,"firebrick4"         , "purple4"          
+              ,"darkorchid1"       ,"darkgoldenrod"     ,   "khaki"          
+              ,"lightgoldenrod3"   ,"steelblue3"        ,   "grey22"         )
+n.pft     = length(pft.names) - 1
+
+pft <- c(5,6,8,9,10,11)
+
+#for(s in 1:length(sites)){
+  s <- 2
+  #Set directories
+  dat.dir    <- paste("/projectnb/cheas/paleon/ED_runs/p1a_spin_040414/",sites[s],"/analy2/",sep="")
+  match.files <- grep("-Y-",list.files(dat.dir))
+  files <- list.files(dat.dir)
+  ann.files  <- files[match.files] #yearly files only
+  
+  #Get time window
+  yeara <- as.numeric(strsplit(ann.files,"-")[[1]][3]) #first year
+  yearz <- as.numeric(strsplit(ann.files,"-")[[length(ann.files)]][3]) #last year
+  
+  agb.pft <- lai.pft <- bsa.pft <- dba.pft <- den.pft <- matrix(nrow=(yearz-yeara+1),ncol=length(pft))
+
+  for (y in yeara:yearz){
+    cat(" - Reading file :",ann.files[y-yeara+1],"...","\n")
+    now <- open.ncdf(paste(dat.dir,ann.files[y-yeara+1],sep=""))
+    
+    #Grab cohort level variables.
+    ipft      <- get.var.ncdf(now,'PFT')
+    dbh       <- get.var.ncdf(now,'DBH')
+    nplant    <- get.var.ncdf(now,'NPLANT')
+    lai       <- get.var.ncdf(now,'LAI_CO')
+    agb       <- get.var.ncdf(now,'AGB_CO')
+    bsa       <- get.var.ncdf(now,'BA_CO')
+    dba_dt    <- get.var.ncdf(now,'DBA_DT')
+    
+    #if any PFTs go extinct, make placeholders for averaging
+    if(length(unique(ipft))<length(pft)){
+      tmp  <- (length(pft)-length(unique(ipft)))
+      ipft <- c(ipft,pft[!(pft %in% ipft)])
+      agb  <- c(agb,rep(0,tmp))
+      lai  <- c(lai,rep(0,tmp))
+      bsa  <- c(bsa,rep(0,tmp))
+      nplant  <- c(nplant,rep(0,tmp))
+      dba_dt <- c(dba_dt,rep(0,tmp))
+    }
+    
+    #PFT summaries
+    agb.pft[(y-yeara+1),] <- tapply(agb,ipft,sum)
+    lai.pft[(y-yeara+1),] <- tapply(lai,ipft,sum)
+    bsa.pft[(y-yeara+1),] <- tapply(bsa,ipft,sum)
+    den.pft[(y-yeara+1),] <- tapply(nplant,ipft,sum)
+    dba.pft[(y-yeara+1),] <- tapply(dba_dt,ipft,mean)
+      
+    close.ncdf(now)
+  }
+  
+  years <- as.character((yeara:yearz)-1000)
+  year.date <- as.Date(years,"%Y")
+#  png(paste(plot.path,sites[s],'_AGBbyPFT','.png',sep=''),width=900,height=600)
+#  pdf(paste(plot.path,sites[s],"_spinup",sep=''))
+  plot(year.date,agb.pft[,1],col=pft.cols[5],pch=16,ylim=range(agb.pft),
+       xlab="spin-up date",ylab="Annual aboveground biomass [kg/m2]",
+       main=paste(site.names[s],": Spin-up",sep=""))
+  for(p in 2:ncol(agb.pft)){
+    points(year.date,agb.pft[,p],col=pft.cols[4+p],pch=16)
+  }
+  legend(year.date[2],max(agb.pft)-mean(agb.pft),pft.names[sort(unique(ipft))],col=pft.cols[5:10],pch=16)
+
+  plot(year.date,lai.pft[,1],col=pft.cols[5],pch=16,ylim=range(lai.pft),
+       xlab="spin-up date",ylab="Annual mean LAI [m2/m2]",
+       main=paste(site.names[s],": Spin-up",sep=""))
+  for(p in 2:ncol(lai.pft)){
+    points(year.date,lai.pft[,p],col=pft.cols[4+p],pch=16)
+  }
+  legend(year.date[2],max(lai.pft,na.rm=TRUE)-mean(lai.pft,na.rm=TRUE),pft.names[sort(unique(ipft))],col=pft.cols[5:10],pch=16)
+  
+  plot(year.date,bsa.pft[,1],col=pft.cols[5],pch=16,ylim=range(bsa.pft),
+       xlab="spin-up date",ylab="Annual sum basal area [cm2/m2]",
+       main=paste(site.names[s],": Spin-up",sep=""))
+  for(p in 2:ncol(bsa.pft)){
+    points(year.date,bsa.pft[,p],col=pft.cols[4+p],pch=16)
+  }
+  legend(year.date[2],max(bsa.pft,na.rm=TRUE)-mean(bsa.pft,na.rm=TRUE),pft.names[sort(unique(ipft))],col=pft.cols[5:10],pch=16)
+  
+  plot(year.date,den.pft[,1],col=pft.cols[5],pch=16,ylim=range(den.pft),
+       xlab="spin-up date",ylab="Annual sum of density [nplant/m2]",
+       main=paste(site.names[s],": Spin-up",sep=""))
+  for(p in 2:ncol(den.pft)){
+    points(year.date,den.pft[,p],col=pft.cols[4+p],pch=16)
+  }
+  legend(year.date[2],max(den.pft,na.rm=TRUE)-mean(den.pft,na.rm=TRUE),pft.names[sort(unique(ipft))],col=pft.cols[5:10],pch=16)
+  
+  plot(year.date,dba.pft[,1],col=pft.cols[5],pch=16,ylim=c(-1,1),
+       xlab="spin-up date",ylab="Annual mean dba_dt",
+       main=paste(site.names[s],": Spin-up",sep=""))
+  for(p in 2:ncol(dba.pft)){
+    points(year.date,dba.pft[,p],col=pft.cols[4+p],pch=16)
+  }
+  legend(year.date[2],max(dba.pft,na.rm=TRUE)-mean(dba.pft,na.rm=TRUE),pft.names[sort(unique(ipft))],col=pft.cols[5:10],pch=16)
+  
+  
